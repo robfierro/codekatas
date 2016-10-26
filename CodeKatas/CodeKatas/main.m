23bc518d4b05de8de4bd1839b8f2509bf22c1b0e
Roberto Fierro Martinez <roberto.fierro.mtz@gmail.com>
2016-10-25 22:30:47 -0400

    Adding Xcode project

---
+++ CodeKatas/CodeKatas/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CodeKatas
+//
+//  Created by Roberto Fierro Martinez on 10/25/16.
+//  Copyright © 2016 Roberto Fierro Martinez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
